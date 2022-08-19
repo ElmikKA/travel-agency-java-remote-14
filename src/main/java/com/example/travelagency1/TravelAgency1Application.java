0cb54b593cf26e2d2daeb7ead0051e079a9be491
elmik <elmik.karl@gmail.com>
2022-08-19 17:02:14 +0300

    [init] - project skeleton

---
+++ src/main/java/com/example/travelagency1/TravelAgency1Application.java
@@ -0,0 +1,13 @@
+package com.example.travelagency1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgency1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgency1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/travelagency1/TravelAgency1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelagency1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgency1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
