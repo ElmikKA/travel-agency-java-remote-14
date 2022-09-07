8debfce2c91b0411a0cdee2a72e7ecbf3ad10668
elmik <elmik.karl@gmail.com>
2022-09-07 14:40:32 +0300

    [change] - SecurityRulesConverter - done

--- backend/src/main/java/com/example/travelagency1/converter/SecurityRulesConverter.java
+++ backend/src/main/java/com/example/travelagency1/converter/SecurityRulesConverter.java
@@ -2,21 +2,19 @@ package com.example.travelagency1.converter;
 
 import com.example.travelagency1.dto.SecurityRulesDto;
 import com.example.travelagency1.entity.SecurityRules;
-import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.stereotype.Component;
 // TODO - implement
 @Component
 public class SecurityRulesConverter implements Converter<SecurityRules, SecurityRulesDto> {
     @Override
     public SecurityRulesDto fromEntityToDto(SecurityRules entity) {
-        //TODO
 
-        return null;
+        return new SecurityRulesDto(entity.getTravelRes(), entity.getInsurance());
     }
 
     @Override
     public SecurityRules fromDtoToEntity(SecurityRulesDto dto) {
-        //TODO
-        return null;
+
+        return new SecurityRules(dto.travelRes(), dto.insurance());
     }
 }
