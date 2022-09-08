d885f7a85c5b6bfd9c62ba6fa55205f62037c216
elmik <elmik.karl@gmail.com>
2022-09-08 13:41:50 +0300

    [change] - providing dummy data for our trips - activated only "develop" profile

--- backend/src/main/java/com/example/travelagency1/repository/TripRepository.java
+++ backend/src/main/java/com/example/travelagency1/repository/TripRepository.java
@@ -1,4 +1,103 @@
 package com.example.travelagency1.repository;
 
-public class TripRepository {
+import com.example.travelagency1.entity.*;
+import com.example.travelagency1.entity.enumeration.MealType;
+import com.example.travelagency1.entity.enumeration.PaymentType;
+import com.example.travelagency1.entity.enumeration.TransportTypes;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+// TripRepository bean will be created only when "develop" profile is activated
+//useful for development purposes
+@Profile("develop")
+@Repository
+public class TripRepository implements CrudRepository<Trip, Long> {
+    @Override
+    public <S extends Trip> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Trip> Iterable<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<Trip> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public List<Trip> findAll() {
+        return List.of(
+                Trip.builder()
+                        .tripStartDate(LocalDate.now().plusDays(1))
+                        .tripEndDate(LocalDate.now().plusDays(15))
+                        .destination(new Destination("UK", "London", "Hilton"))
+                        .tripPrice(new Price(BigDecimal.valueOf(500L), "EUR"))
+                        .typeOfTransport(TransportTypes.PLANE)
+                        .rulesOfSecurity(new SecurityRules("", ""))
+                        .typeOfPayment(PaymentType.PAYPAL)
+                        .typeOfMeal(MealType.ALL_INCLUDED)
+                        .hotelFacilities(new HotelFacilities(List.of(), List.of("Bathroom", "KingBed")))
+                        .photos(List.of("https://fastly.4sqi.net/img/general/600x600/1376806605_Hc3gxR3iMMI5OKRpt7ook-zGGreT4XxgEapWx0wC844.jpg"))
+                        .build(),
+                Trip.builder()
+                        .tripStartDate(LocalDate.now().plusDays(1))
+                        .tripEndDate(LocalDate.now().plusDays(15))
+                        .destination(new Destination("Thailand", "Bangkog", "Sawasdee Sunshine"))
+                        .tripPrice(new Price(BigDecimal.valueOf(1_500L), "BAHT"))
+                        .typeOfTransport(TransportTypes.PLANE)
+                        .rulesOfSecurity(new SecurityRules("", ""))
+                        .typeOfPayment(PaymentType.CARD)
+                        .typeOfMeal(MealType.ALL_INCLUDED)
+                        .hotelFacilities(new HotelFacilities(List.of(), List.of("Bed", "Swimming Pool", "Bathroom", "Parking")))
+                        .photos(List.of("https://pix10.agoda.net/hotelImages/6796247/0/33adac49e47c732b38c54faca72b84f9.jpg?ca=22&ce=0&s=1024x768"))
+                        .build());
+    }
+
+    @Override
+    public Iterable<Trip> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(Trip entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Trip> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
